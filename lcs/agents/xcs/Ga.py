acf82e91f406788367b9ec17818715c47c4010f5
Patryk Kostrzewa <p.kostrzewa@mcr-inc.pl>
2019-07-04 23:20:10 +0200

    Implemented some GA

--- lcs/agents/xcs/Ga.py
+++ lcs/agents/xcs/Ga.py
@@ -1,12 +1,35 @@
+from .Constants import Constants
+from .Random import Random
+
 class GA:
-    def __selectOffspring(self):
-        return "TODO"
+
+    # --- PRIVATE ---
+
+    def __selectOffspring(self, action_set):
+        targets = []
+
+        for cl in action_set:
+            targets.append(cl)
+
+        fitnesses = []
+        if 0 < Constants.tau <= 1.0:
+            for cl in action_set:
+                fitnesses.append((cl.fitness, cl.numerosity))
+            selected = Random.tournament_selection(fitnesses, Constants.tau)
+        else:
+            for cl in action_set:
+                fitnesses.append(cl.fitness)
+            selected = Random.roulette_wheel_selection(fitnesses)
+
+        return
 
     def __crossover(self):
         return "TODO"
 
     def __mutate(self):
         return "TODO"
+
+    # --- PUBLIC ---
 
     def run(self):
         return "TODO"
